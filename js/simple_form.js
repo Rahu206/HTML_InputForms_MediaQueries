be8e11900b6834b071329324550897769a347634
Rahu206 <rahuldeshpande93@gmail.com>
2021-09-04 00:59:19 +0530

    UC2_added: html form for validation of email

---
+++ js/simple_form.js
@@ -0,0 +1,40 @@
+/**
+ As a User need to enter a valid email 
+    - E.g. abc.xyz@bl.co.in 
+    - Email has 3 mandatory parts (abc, bl & co) and 2 optional (xyz & in) with precise @ and . positions
+ */
+
+//UC1- Validating First Name
+const text = document.querySelector("#text");
+const textError = document.querySelector(".text-error");
+text.addEventListener("input", function () {
+  let nameRange = RegExp("^[A-Z]{1}[a-z]{2,}$");
+  if (nameRange.test(text.value)) {
+    textError.textContent = "";
+  } else {
+    textError.textContent = "Name is incorrect";
+  }
+});
+
+/**
+ * Update the salary value when scrolled from left to right and vice-versa
+ */
+
+const salary = document.querySelector("#salary");
+const output = document.querySelector(".salary-output");
+output.textContent = salary.value;
+salary.addEventListener("input", function () {
+  output.textContent = salary.value;
+});
+/**
+ * To validate the email field using the REGEX expression
+ */
+
+ const email = document.querySelector('#email');
+ const emailError = document.querySelector('.email-error');
+ text.addEventListener('input', function() {
+     let emailRegex = RegExp("^[A-Za-z0-9!#$%&*+\=?`{|}~^-]+(?:\.[A-Za-z!#$%&*+\=?`{|}~^-]+)*@(?:([0-9-]{1}|[a-zA-Z]{3,5})\.)+[a-zA-Z]{2,3}$");
+     if(emailRegex.test(email.value))
+         emailError.emailContent = "";
+     else emailError.emailContent = "Email is Incorrect";
+ });
